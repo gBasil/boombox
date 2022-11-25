754412fa8bd27c93764e03e24c2defffc85af1f7
Basil <basil@gbasil.dev>
2022-11-25 14:20:07 -0700

    📈 Added Umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,14 @@ const Document = () => (
 	<Html>
 		<Head>
 			<link rel='icon' href='/favicon.ico' />
+			<script
+				async
+				defer
+				data-website-id='d4a35a5f-a8c9-44cb-9519-3a69de2bda29'
+				data-do-not-track='true'
+				data-domains='boombox.gbasil.dev'
+				src='https://speedcore.gbasil.dev/umami.js'
+			/>
 		</Head>
 		<body className='mx-2 bg-darkestGreen font-inter text-lightestGreen'>
 			<Main />
