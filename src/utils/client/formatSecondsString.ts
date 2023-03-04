b3d0619ff891ae3abc18e62eb14e0fc81e2c47c6
Basil <basil@basil.cafe>
2023-03-03 20:14:12 -0700

    🐛 Fixed total duration displaying incorrect time

--- src/utils/client/formatSecondsString.ts
+++ src/utils/client/formatSecondsString.ts
@@ -1,14 +1,16 @@
 const s = (i: number, string: string) => `${i} ${string}${i === 1 ? '' : 's'}`;
 
 function formatSeconds(seconds: number): string {
-	const day = Math.floor(seconds / (3600 * 24));
-	const hr = Math.floor(seconds / 3600);
-	const min = Math.floor(seconds % 3600 / 60);
-	const sec = Math.floor(seconds % 3600 % 60);
+	const days         = Math.floor(seconds / 60 / 60 / 24);
+	const hoursInDay   = Math.floor(seconds / 60 / 60 % 24);
+	const hr           = Math.floor(seconds / 60 / 60);
+	const min          = Math.floor(seconds / 60);
+	const sec          = Math.floor(seconds);
 
-	if (day > 0) return s(sec, 'day');
-	if (hr > 0) return s(hr, 'hour');
-	if (min > 0) return s(min, 'minute');
+	if (days > 0) return s(days, 'day') + ' and ' + s(hoursInDay, 'hour');
+	if (hr   > 0) return s(hr,   'hour');
+	if (min  > 0) return s(min,  'minute');
+	if (sec  > 0) return s(sec,  'second');
 	return s(sec, 'second');
 }
 
